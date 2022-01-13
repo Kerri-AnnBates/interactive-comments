8d75208326aa0abf3bff764b6072fc63036e9ea1
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-01-13 13:17:08 -0500

    completing comment structure

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -3,10 +3,12 @@ import React from 'react';
 const Comment = () => {
     return (
         <div className='comment'>
-            <div className='comment-details'>Author Date</div>
-            <div className='comment-text'>Comment Text</div>
-            <div className='user-action'>User Actions</div>
-            <div className='vote'>Votes</div>
+            <div className='comment-details'><span className='avatar'>img</span> <span className='username'>amyrobson</span> <span className='created-date'>1 month ago</span></div>
+            <div className='comment-text'><p>Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.</p></div>
+            <div className='vote'>
+                <div className='mobile-vote'><button>+</button> 12 <button>-</button></div>
+            </div>
+            <div className='user-actions'><span>Reply</span></div>
         </div>
     )
 }
