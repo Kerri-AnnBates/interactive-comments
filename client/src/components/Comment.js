cbbd1254fefe636b20bf0cf12c520c65b8120f3a
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-01-15 21:30:00 -0500

    style comment author section

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import avatar from '../images/avatars/image-amyrobson.png';
 
 const Comment = () => {
     return (
         <div className='comment'>
-            <div className='comment-details'><span className='avatar'>img</span> <span className='username'>amyrobson</span> <span className='created-date'>1 month ago</span></div>
+            <div className='comment-details'><span className='avatar'><img alt='author profile picture' src={avatar} /></span> <span className='username'>amyrobson</span> <span className='created-date'>1 month ago</span></div>
             <div className='comment-text'><p>Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.</p></div>
             <div className='vote'>
                 <div className='mobile-vote'><button>+</button> 12 <button>-</button></div>
