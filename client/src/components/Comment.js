e1a094d1f0909be48ab3faf9d9e09bd9f6ab95f7
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-01-15 21:47:17 -0500

    style vote buttons

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import avatar from '../images/avatars/image-amyrobson.png';
+import Vote from './Vote';
 
 const Comment = () => {
     return (
         <div className='comment'>
-            <div className='comment-details'><span className='avatar'><img alt='author profile picture' src={avatar} /></span> <span className='username'>amyrobson</span> <span className='created-date'>1 month ago</span></div>
-            <div className='comment-text'><p>Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.</p></div>
-            <div className='vote'>
-                <div className='mobile-vote'><button>+</button> 12 <button>-</button></div>
+            <div className='content-container'>
+                <div className='comment-details'><span className='avatar'><img alt='author profile picture' src={avatar} /></span> <span className='username'>amyrobson</span> <span className='created-date'>1 month ago</span></div>
+                <div className='comment-text'><p>Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.</p></div>
             </div>
+
+            <Vote />
             <div className='user-actions'><span>Reply</span></div>
         </div>
     )
---
+++ client/src/components/Vote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Vote = () => {
+    return (
+        <div className='vote'>
+            <div className='mobile-vote'><button>+</button> 12 <button>-</button></div>
+        </div>
+    )
+}
+
+export default Vote;
