ab79a91642118492044bdf7212f775a7d4dbc90f
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-01-17 00:03:38 -0500

    setup incrementing vote count

--- client/src/components/Vote.js
+++ client/src/components/Vote.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Vote = ({ vote }) => {
+    const [voteCount, setVoteCount] = useState(vote);
+
+    const handleVoteCount = (e) => {
+        const aciton = e.target.dataset.value;
+
+        if (aciton === 'plus') {
+            setVoteCount((vote) => vote + 1);
+        }
+
+        if (aciton === 'minus') {
+            setVoteCount((vote) => vote - 1);
+        }
+    }
+
     return (
         <div className='vote'>
-            <div className='vote-container'><button>+</button>{vote}<button>-</button></div>
+            <div className='vote-container'><button onClick={handleVoteCount} data-value='plus'>+</button>{voteCount}<button onClick={handleVoteCount} data-value='minus'>-</button></div>
         </div>
     )
 }
