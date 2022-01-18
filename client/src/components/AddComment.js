0b11e9692941b207c13726b3834e8208d70305ec
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-01-18 09:51:39 -0500

    add comment editor

---
+++ client/src/components/AddComment.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddComment = ({ currentUser }) => {
+    return (
+        <div className='comment-editor'>
+            <textarea defaultValue='Add a comment...'></textarea>
+            <div className='avatar'><img alt='author profile picture' src={currentUser?.image.png} /></div>
+            <button>Send</button>
+        </div>
+    )
+}
+
+export default AddComment;
