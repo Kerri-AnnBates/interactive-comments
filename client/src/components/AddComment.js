8cdc0efe13dcba173d6f6e684bf5613feecbecf4
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-01-20 13:44:28 -0500

    set up controlled input'

--- client/src/components/AddComment.js
+++ client/src/components/AddComment.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddComment = ({ currentUser }) => {
+    const [commentVal, setCommentVal] = useState('');
+
+    const handleCommentChange = (e) => {
+        setCommentVal(e.target.value);
+    }
+
     return (
         <div className='comment-editor'>
-            <textarea defaultValue='Add a comment...'></textarea>
+            <form>
+                <textarea name="comment" value={commentVal} onChange={handleCommentChange} placeholder='Add a comment...'></textarea>
+                <button className='primary-btn'>Send</button>
+            </form>
             <div className='avatar'><img alt='author profile picture' src={currentUser?.image.png} /></div>
-            <button className='primary-btn'>Send</button>
-        </div>
+        </div >
     )
 }
 
