3d7edd1d6e50f78ecbbd0ee3066a21932a6f27b9
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-01-25 17:05:38 -0500

    fix dynamic user actions

--- client/src/components/UserActions.js
+++ client/src/components/UserActions.js
@@ -7,11 +7,12 @@ import editIcon from '../images/icon-edit.svg';
 const UserActions = ({ currentUser, user }) => {
     return (
         <div className='user-actions'>
-            {(currentUser.username === user.username) && (<>
-                <span><img alt='reply icon' src={editIcon} /> Edit</span>
-                <span className='delete'><img alt='reply icon' src={deleteIcon} /> Delete</span>
-            </>)}
-            <span><img alt='reply icon' src={replyIcon} /> Reply</span>
+            {(currentUser.username === user.username) ?
+                (<>
+                    <span><img alt='reply icon' src={editIcon} /> Edit</span>
+                    <span className='delete'><img alt='reply icon' src={deleteIcon} /> Delete</span>
+                </>) :
+                (<span><img alt='reply icon' src={replyIcon} /> Reply</span>)}
         </div>
     )
 }
