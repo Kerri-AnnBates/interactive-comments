6be782bf771e381442ebfe4c3758b3b63b5c967e
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-02-01 00:06:27 -0500

    style modal buttons

--- client/src/components/DeleteModal.js
+++ client/src/components/DeleteModal.js
@@ -25,8 +25,8 @@ const DeleteModal = (props) => {
                 <h3 className='modal-header'>Delete comment</h3>
                 <p>Are you sure you want to delete this comment? This will remove the comment and can't be undone.</p>
                 <div className='modal-actions'>
-                    <button onClick={handleCancel}>No, cancel</button>
-                    <button onClick={handleDelete}>Yes, Delete</button>
+                    <button className='secondary-btn' onClick={handleCancel}>No, cancel</button>
+                    <button className='danger-btn' onClick={handleDelete}>Yes, Delete</button>
                 </div>
             </div>
         </div>
