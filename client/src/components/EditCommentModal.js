5b347353573dcb2a66d90aee3c1479b73c8759f2
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-02-23 18:24:01 -0500

    complete edit for comment

--- client/src/components/EditCommentModal.js
+++ client/src/components/EditCommentModal.js
@@ -14,8 +14,25 @@ const EditCommentModal = ({ setIsEditModalOpen, commentToEditId }) => {
         }
     }, []);
 
-    const editContent = (id) => {
+    const editContent = (e) => {
+        e.preventDefault();
 
+        if (commentsData) {
+            let commentFound = commentsData.comments.find(comment => comment.id === commentToEditId);
+            commentFound = { ...commentFound, content: input }
+
+            const currComments = commentsData.comments.map(currComm => {
+                if (currComm.id === commentToEditId) {
+                    Object.assign(currComm, commentFound);
+                }
+
+                return currComm;
+            });
+
+            setCommentsData({ ...commentsData, comments: currComments });
+        }
+
+        setIsEditModalOpen(false);
     }
 
     const handleCommentChange = (e) => {
@@ -26,14 +43,10 @@ const EditCommentModal = ({ setIsEditModalOpen, commentToEditId }) => {
         setIsEditModalOpen(false);
     }
 
-    const handleSubmit = () => {
-
-    }
-
     return (
         <div className='modal'>
             <div className='modal-container'>
-                <form>
+                <form onSubmit={editContent}>
                     <textarea name='edit' className='edit-comment' value={input} onChange={handleCommentChange}></textarea>
                     <button className='secondary-btn' onClick={handleCancel}>No, cancel</button>
                     <button className='danger-btn'>Update</button>
