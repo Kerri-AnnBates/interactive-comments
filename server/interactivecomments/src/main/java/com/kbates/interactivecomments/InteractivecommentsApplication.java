521303e1bf980218153fefcd6306967b2a85a824
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-04-11 15:21:47 -0400

    init server commit

---
+++ server/interactivecomments/src/main/java/com/kbates/interactivecomments/InteractivecommentsApplication.java
@@ -0,0 +1,13 @@
+package com.kbates.interactivecomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractivecommentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractivecommentsApplication.class, args);
+	}
+
+}
---
+++ server/interactivecomments/src/test/java/com/kbates/interactivecomments/InteractivecommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbates.interactivecomments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractivecommentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
