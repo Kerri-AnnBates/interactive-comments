12e52582cacceb9ad0aeb177deca04e0b90aa543
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-04-22 16:50:15 -0400

    setup user entity

---
+++ server/interactivecomments/src/main/java/com/kbates/interactivecomments/User/User.java
@@ -0,0 +1,48 @@
+package com.kbates.interactivecomments.User;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userId;
+    private String username;
+    private String image;
+
+    public User() {}
+
+    public User(long userId, String username, String image) {
+        this.userId = userId;
+        this.username = username;
+        this.image = image;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
