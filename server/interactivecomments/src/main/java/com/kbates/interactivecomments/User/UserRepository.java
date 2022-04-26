d8459f0f0168da9876ac4a0d47b517392f6412ad
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-04-26 17:36:28 -0400

    user repo

---
+++ server/interactivecomments/src/main/java/com/kbates/interactivecomments/User/UserRepository.java
@@ -0,0 +1,6 @@
+package com.kbates.interactivecomments.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
