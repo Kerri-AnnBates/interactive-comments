153e46c00089dc65367d502416945fadd3dd6bdd
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-04-29 15:39:31 -0400

    setup comment mvc structure

---
+++ server/interactivecomments/src/main/java/com/kbates/interactivecomments/comment/CommentService.java
@@ -0,0 +1,4 @@
+package com.kbates.interactivecomments.comment;
+
+public interface CommentService {
+}
---
+++ server/interactivecomments/src/main/java/com/kbates/interactivecomments/comment/CommentServiceImpl.java
@@ -0,0 +1,7 @@
+package com.kbates.interactivecomments.comment;
+
+import org.springframework.stereotype.Service;
+
+@Service(value = "commentService")
+public class CommentServiceImpl implements CommentService {
+}
