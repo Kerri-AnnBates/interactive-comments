1a04cc5a5fe6f702d8ff7ca56378f602e24db01c
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-06-04 16:54:18 -0400

    seed db with users

--- server/interactivecomments/src/main/java/com/kbates/interactivecomments/SeedData.java
+++ server/interactivecomments/src/main/java/com/kbates/interactivecomments/SeedData.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class SeedData implements CommandLineRunner {
     @Autowired
@@ -15,10 +17,29 @@ public class SeedData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        // Create current user
+        // Get current date
+        LocalDate today = LocalDate.now();
+
+        // Create current users
         User user1 = new User();
+        User user2 = new User();
+        User user3 = new User();
+        User user4 = new User();
+
         user1.setUsername("juliusomo");
-        juliusomo.setImage("./images/avatars/image-juliusomo.png");
-        juliusomo.getComments().add(new Comment());
+        user1.setImage("./images/avatars/image-juliusomo.png");
+        userService.addUser(user1);
+
+        user2.setUsername("amyrobson");
+        user2.setImage("./images/avatars/image-amyrobson.png");
+        userService.addUser(user2);
+
+        user3.setUsername("maxblagun");
+        user3.setImage("./images/avatars/image-maxblagun.png");
+        userService.addUser(user3);
+
+        user4.setUsername("ramsesmiron");
+        user4.setImage("./images/avatars/image-ramsesmiron.png");
+        userService.addUser(user4);
     }
 }
