70f2eae7d9c5b75f248d606a5e6f3cd77aeba56b
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-06-05 14:32:27 -0400

    set up a couple comment services

--- server/interactivecomments/src/main/java/com/kbates/interactivecomments/comment/CommentService.java
+++ server/interactivecomments/src/main/java/com/kbates/interactivecomments/comment/CommentService.java
@@ -1,4 +1,9 @@
 package com.kbates.interactivecomments.comment;
 
+import java.util.List;
+
 public interface CommentService {
+    List<Comment> getAllComments();
+    List<Comment> getCommentsById(Long id);
+    Comment addComment(Comment comment);
 }
--- server/interactivecomments/src/main/java/com/kbates/interactivecomments/comment/CommentServiceImpl.java
+++ server/interactivecomments/src/main/java/com/kbates/interactivecomments/comment/CommentServiceImpl.java
@@ -1,7 +1,34 @@
 package com.kbates.interactivecomments.comment;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service(value = "commentService")
 public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    public CommentServiceImpl(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public List<Comment> getAllComments() {
+        List<Comment> listOfComments = commentRepository.findAll();
+
+        return listOfComments;
+    }
+
+    @Override
+    public List<Comment> getCommentsById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Comment addComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
 }
