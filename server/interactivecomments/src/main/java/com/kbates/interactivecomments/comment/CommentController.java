12b85b9b358d71d64d6ae26bd14f83f649c4151b
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-06-13 21:25:32 -0400

    get comments

--- server/interactivecomments/src/main/java/com/kbates/interactivecomments/comment/CommentController.java
+++ server/interactivecomments/src/main/java/com/kbates/interactivecomments/comment/CommentController.java
@@ -1,4 +1,26 @@
 package com.kbates.interactivecomments.comment;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/comments")
 public class CommentController {
+    private final CommentService commentService;
+
+    public CommentController(final CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Comment>> getAllComments() {
+        List<Comment> commentsList = commentService.getAllComments();
+
+        return new ResponseEntity<List<Comment>>(commentsList, HttpStatus.OK);
+    }
 }
