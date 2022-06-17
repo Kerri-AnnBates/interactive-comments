fe17ee80d8ddffc742e0d448f61c855451f47888
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-06-16 22:12:13 -0400

    comment post request

--- server/interactivecomments/src/main/java/com/kbates/interactivecomments/comment/CommentController.java
+++ server/interactivecomments/src/main/java/com/kbates/interactivecomments/comment/CommentController.java
@@ -2,10 +2,7 @@ package com.kbates.interactivecomments.comment;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +25,13 @@ public class CommentController {
     @GetMapping("/{id}")
     public ResponseEntity<Comment> getCommentById(@PathVariable Long id) {
         Comment comment = commentService.findCommentById(id);
+
+        return new ResponseEntity<Comment>(comment, HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Comment> addComment(@RequestBody Comment comment) {
+        Comment newComment = commentService.addNewComment(comment);
 
         return new ResponseEntity<Comment>(comment, HttpStatus.OK);
     }
