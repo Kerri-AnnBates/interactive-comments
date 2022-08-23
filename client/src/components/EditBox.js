c30350a6273ed47245dd99dfa89480596327fd82
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-08-22 20:38:54 -0400

    handle text change

--- client/src/components/EditBox.js
+++ client/src/components/EditBox.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function EditBox(props) {
-    const { content } = props;
+    const { replyingTo, content } = props;
+
+    const [val, setVal] = useState(content);
+
+    const handleOnChange = (e) => {
+        setVal(e.target.value);
+    }
 
     return (
         <div className='edit-container'>
-            <textarea value={content}></textarea>
+            {replyingTo && (<p>Replying to <span className='reply-username'>@{replyingTo}</span></p>)}
+            <textarea value={val} onChange={handleOnChange}></textarea>
             <button className='primary-btn'>Save</button>
         </div>
     )
