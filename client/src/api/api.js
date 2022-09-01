45670f2de512f1fcac957bb6aca28d17f990c564
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-08-31 20:59:26 -0400

    add new requests

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -12,6 +12,10 @@ export const getCommentById = (id) => {
     return axios.get(`/comments/${id}`).then(res => res).catch(err => err);
 }
 
+export const getReplyById = (id) => {
+    return axios.get(`/replies/${id}`).then(res => res).catch(res => res);
+}
+
 export const deleteComment = (id) => {
     return axios.delete(`/comments/${id}`).then(res => res).catch(err => err);
 }
@@ -26,4 +30,8 @@ export const addComment = (comment) => {
 
 export const addReplyToComment = (reply, commentId) => {
     return axios.post(`/replies/comment/${commentId}`, reply).then(res => res).catch(err => err);
+}
+
+export const updateComment = (id, comment) => {
+    return axios.put(`/comments/${id}`, comment).then(res => res).catch(err => err);
 }
