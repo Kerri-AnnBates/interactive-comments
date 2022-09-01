6b51ef28bef798d9fd0c36d1ac87ea4719dad6c8
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-08-31 21:25:35 -0400

    functional update comment

--- client/src/components/EditBox.js
+++ client/src/components/EditBox.js
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { getCommentById, updateComment } from '../api/api';
 
 function EditBox(props) {
-    const { replyingTo, content } = props;
+    const { replyingTo, id } = props;
 
-    const [val, setVal] = useState(content);
+    const [comment, setComment] = useState(null);
+
+    // TODO: Need to make it work for replies also.
+
+    useEffect(() => {
+        getCommentById(id).then(({ data }) => {
+            setComment(data);
+        }).catch(err => console.log(err));
+    }, []);
 
     const handleOnChange = (e) => {
-        setVal(e.target.value);
+        setComment({ ...comment, content: e.target.value });
+    }
+
+    const handleSubmit = () => {
+        updateComment(id, comment).then(res => {
+            console.log(res);
+        }).catch(err => console.log(err));
     }
 
     return (
         <div className='edit-container'>
             {replyingTo && (<p>Replying to <span className='reply-username'>@{replyingTo}</span></p>)}
-            <textarea value={val} onChange={handleOnChange}></textarea>
-            <button className='primary-btn'>Save</button>
+            <textarea value={comment?.content} onChange={handleOnChange}></textarea>
+            <button className='primary-btn' onClick={handleSubmit}>Save</button>
         </div>
     )
 }
