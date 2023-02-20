0a0ffdd4fc6f93675f635a1456393fd87c18f9c3
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-02-20 14:17:35 -0500

    minor change

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const domain = process.env.REACT_APP_ENDPOINT ? process.env.REACT_APP_ENDPOINT : `localhost:2019`;
+const domain = process.env.REACT_APP_ENDPOINT ? process.env.REACT_APP_ENDPOINT : `localhost:8080`;
 const protocol = process.env.REACT_APP_PROTOCOL ? `${process.env.REACT_APP_PROTOCOL}:` : window.location.protocol;
 const baseUrl = `${protocol}//${domain}/api`;
 
